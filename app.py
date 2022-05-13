4079a28be0a725ce05f020c2832d8ccd5f3a076d
Austin Donlevy <adonlevy2@gmail.com>
2022-05-13 17:29:24 -0400

    basic routing for two regions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/kanto', methods=['GET'])
+def kanto():
+    return render_template('region.html', region="Kanto")
+
+
+@app.route('/johto', methods=['GET'])
+def johto():
+    return render_template('region.html', region="Johto")
 
 
 if __name__ == '__main__':

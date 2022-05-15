345f2ed91f716aca1536bbb9f0aa970a2b3b932f
Austin Donlevy <adonlevy2@gmail.com>
2022-05-15 18:08:09 -0400

    time file to compare API response speed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 First app using Flask that is experimenting with API calls and accessing data within those structures
 
-API calls are extremely slow but that's not the point (for now)
+Implemented multiprocessing on API calls to speed up response time. Initially I had one call after another which took 25.08 seconds to return all 151 pokemon from generation 1. After implementing multiprocessing the same aPI call took 3.5 seconds. You can test the response spee in the 'time.py' file.
 
 Search function returns a single card with whatever pokemon you searched for
--- time.py
+++ time.py
@@ -1,13 +1,31 @@
-import requests
+import time
 from multiprocessing import Pool
+import requests
 
-list_of_pokemon = []
+
+def get_pokemon_serially(numbers):
+    start = time.time()
+    result = []
+    for i in numbers:
+        result.append(get_pokemon(i))
+    end = time.time() - start
+    print(f"Serial processing took {end} seconds")
 
 
-def get_pokemon(i):
+def get_pokemon_with_mp(numbers):
+    start = time.time()
+    p = Pool()
+    result = p.map(get_pokemon, numbers)
+    p.close()
+    p.join()
+    end = time.time() - start
+    print(f"Multiprocessing took {end} seconds")
+
+
+def get_pokemon(x):
     # Contact API
     try:
-        url = f"https://pokeapi.co/api/v2/pokemon/{i+1}/"
+        url = f"https://pokeapi.co/api/v2/pokemon/{x+1}/"
         response = requests.get(url)
         response.raise_for_status()
     except requests.RequestException:
@@ -24,25 +42,13 @@ def get_pokemon(i):
             "weight": info["weight"],
             "sprite": info["sprites"]["front_default"]
         }
-
-        # append the info to a new list that I will return
-
-       # print(list_of_pokemon)
-
     except (KeyError, TypeError, ValueError):
         return None
-
-# return the list of pokemon and the values I want
     return pokemon_info
 
 
 if __name__ == '__main__':
-    p = Pool()
-    numbers = range(50)
-    result = p.map(get_pokemon, numbers)
+    numbers = range(150)
 
-    p.close()
-    p.join()
-    print(result)
-    for i in range(len(result)):
-        print(result[i]["pokedex"])
+    get_pokemon_serially(numbers)
+    get_pokemon_with_mp(numbers)
